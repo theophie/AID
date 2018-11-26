b7f0e948e78f803495cae6cace46bd187a7a2b28
theophie <45369916+theophie@users.noreply.github.com>
2018-11-26 20:20:19 +0100

    added pressure sensors

--- airDrum.ino
+++ airDrum.ino
@@ -1,5 +1,11 @@
-const int legBuzzer = 13; //buzzer to arduino pin 13
-const int snareBuzzer = 12; //buzzer to arduino pin 13
+const int pedalBuzzer = 13; //buzzer to arduino pin 13
+const int snareBuzzer = 12; //buzzer to arduino pin 12
+const int crushBuzzer = 11; //buzzer to arduino pin 12
+
+const int pedalSensor = 0;
+const int snareSensor = 1;
+const int crushSensor = 2;
+
 char val; // Data received from the serial port
 
 void setup(){
@@ -9,6 +15,11 @@ Serial.begin(9600); // Start serial communication at 9600 bps
 }
 
 void loop(){
+  pS = analogRead(pedalSensor);
+  sS = analogRead(snareSensor);
+  aS = analogRead(crushSensor);
+  //we need to send which sensor was used and the pressure that it was used (sensorName,pressure)
+  
  if (Serial.available()) 
    { // If data is available to read,
      val = Serial.read(); // read it and store it in val
@@ -18,13 +29,17 @@ void loop(){
         delay(500);
         noTone(snareBuzzer);   
        } else if (val == '2') {
-          tone(legBuzzer, 4000);  // Stop sound...
+          tone(pedalBuzzer, 4000);  // Stop sound...
           delay(500);
-          noTone(legBuzzer);    
+       }  noTone(crushBuzzer);  
+          else if (val == '2') {
+          tone(crushBuzzer, 4000);  // Stop sound...
+          delay(500);
+          noTone(crushBuzzer);     
        }else{
-          noTone(legBuzzer);  
+          noTone(pedalBuzzer);  
           noTone(snareBuzzer);  
+           noTone(crushBuzzer);  
         }
-       
    }
 }
