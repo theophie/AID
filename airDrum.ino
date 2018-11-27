5360f61c26e4e24e202ed21f575f05a5484c69c4
theophie <45369916+theophie@users.noreply.github.com>
2018-11-27 15:31:04 +0100

    renamed crush to hihat

--- airDrum.ino
+++ airDrum.ino
@@ -1,15 +1,15 @@
 const int pedalBuzzer = 13; //buzzer to arduino pin 13
 const int snareBuzzer = 12; //buzzer to arduino pin 12
-const int crushBuzzer = 11; //buzzer to arduino pin 12
+const int hiHatBuzzer = 11; //buzzer to arduino pin 11
 
 const int pedalSensor = 0;
 const int snareSensor = 1;
-const int crushSensor = 2;
+const int hiHatSensor = 2;
 
 char val; // Data received from the serial port
 
 void setup(){
-pinMode(pedalBuzzer, OUTPUT); // Set buzzer - pin 9 as an output
+pinMode(pedalBuzzer, OUTPUT); // Set buzzer as an output
 pinMode(snareBuzzer, OUTPUT);
 Serial.begin(9600); // Start serial communication at 9600 bps
 }
@@ -17,7 +17,7 @@ Serial.begin(9600); // Start serial communication at 9600 bps
 void loop(){
 //  pS = analogRead(pedalSensor);
   //sS = analogRead(snareSensor);
-  //aS = analogRead(crushSensor);
+  //aS = analogRead(hiHatSensor);
   //we need to send which sensor was used and the pressure that it was used (sensorName,pressure)
   
  if (Serial.available()) 
@@ -25,21 +25,22 @@ void loop(){
      val = Serial.read(); // read it and store it in val
       if (val == '1') 
        { // If 1 was received
-        tone(snareBuzzer, 4000); // Send 1KHz sound signal...
+        tone(snareBuzzer, 4000); // Send 4KHz sound signal...
         delay(500);
-        noTone(snareBuzzer);   
+        noTone(snareBuzzer);    // Stop sound...
        } else if (val == '2') {
-          tone(pedalBuzzer, 4000);  // Stop sound...
+          tone(pedalBuzzer, 4000);  
           delay(500);
-       }  noTone(crushBuzzer);  
+          noTone(pedalBuzzer);   
+       }  
 
    }else if (val == '3') {
-          tone(crushBuzzer, 4000);  // Stop sound...
+          tone(hiHatBuzzer, 4000); 
           delay(500);
-          noTone(crushBuzzer);     
+          noTone(hiHatBuzzer);     
        }else{
           noTone(pedalBuzzer);  
           noTone(snareBuzzer);  
-           noTone(crushBuzzer);  
+           noTone(hiHatBuzzer);  
         }
    }
